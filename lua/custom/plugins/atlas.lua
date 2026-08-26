8c84696963ef0fda8d84c7469a4f3e927e43293e
Vitor Faiante <vfaiante@uw.co.uk>
2026-08-26 17:31:34 +0100

    refactor: use diffview-plus

--- lua/custom/plugins/atlas.lua
+++ lua/custom/plugins/atlas.lua
@@ -5,54 +5,80 @@ return {
     'nvim-tree/nvim-web-devicons', -- optional but recommended
     'MeanderingProgrammer/render-markdown.nvim', -- optional but recommended
     'esmuellert/codediff.nvim', -- optional (PullRequest diff)
-    'sindrets/diffview.nvim', -- optional; or "dlyongemallo/diffview-plus.nvim"
+    -- 'sindrets/diffview.nvim', -- optional; or "dlyongemallo/diffview-plus.nvim"
+    'dlyongemallo/diffview-plus.nvim',
   },
   -- See Configuration below
   opts = {
     ui = {
       picker = 'snacks',
     },
+
+    keymaps = {
+      pulls = {
+        review = {
+          comment_templates = '<C-t>',
+        },
+      },
+    },
+
+    providers = {
+      ---@type AtlasGitHubConfig
+      github = {
+        cache_ttl = 300,
+      },
+    },
+
     pulls = {
+      comment_templates = {
+        insert_mode = false,
+      },
+
+      github = {
+        ---@type AtlasGitHubViewConfig[]
+        views = {
+          {
+            name = 'My PRs',
+            key = '1',
+            layout = 'plain',
+            search = 'author:@me sort:updated-desc',
+          },
+          {
+            name = 'Energy Team',
+            key = '2',
+            layout = 'compact',
+            search = 'org:utilitywarehouse team:energy sort:updated-desc',
+          },
+        },
+
+        bookmarks = {
+          key = 'S', -- default
+          label = 'Search', -- default
+          items = {
+            ['Drafts'] = 'is:pr is:draft author:@me',
+            ['Recently merged'] = 'is:pr is:merged author:@me sort:updated-desc',
+            ['Review requested'] = 'is:pr is:open review-requested:@me',
+            ['Involves Me'] = 'is:pr involves:@me state:open archived:false sort:updated-desc',
+          },
+        },
+      },
+
       default_merge_method = 'squash',
 
       diff = {
         -- Any command that accepts explicit <base>...<head> Git revisions.
-        open_cmd = 'CodeDiff', -- default; for example "DiffviewOpen" or "CodeDiff".
+        open_cmd = 'AtlasDiff', -- default; for example "AtlasDiff", "DiffviewOpen" or "CodeDiff".
         show_review_panel = true, -- Set true to show the review panel when a diff opens.
         comment_display = 'virtual_lines', -- "virtual_lines" or compact "virtual_text" hints.
+        layout = 'side-by-side', -- "inline" or "side-by-side".
       },
 
-      -- See Pulls Configuration below.
-      providers = {
-        ---@type AtlasGitHubConfig
-        github = {
-          cache_ttl = 300,
-
-          ---@type AtlasGitHubViewConfig[]
-          views = {
-            {
-              name = 'My PRs',
-              key = '1',
-              layout = 'plain',
-              search = 'author:@me sort:updated-desc',
-            },
-            {
-              name = 'Energy Team',
-              key = '2',
-              layout = 'compact',
-              search = 'org:utilitywarehouse team:energy sort:updated-desc',
-            },
-          },
-
-          bookmarks = {
-            key = 'S', -- default
-            label = 'Search', -- default
-            items = {
-              ['Drafts'] = 'is:pr is:draft author:@me',
-              ['Recently merged'] = 'is:pr is:merged author:@me sort:updated-desc',
-              ['Review requested'] = 'is:pr is:open review-requested:@me',
-            },
-          },
+      repo_config = {
+        -- Maps `workspace/repo` to local paths. Used for checkout, diffs, and custom actions.
+        paths = {
+          ['utilitywarehouse/*'] = '~/code/uw/*',
+          ['vitorf7/dotfiles'] = '~/dotfiles',
+          ['vitorf7/nvim-kick'] = '~/nvim-kick',
         },
       },
     },
--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,6 +1,20 @@
 return {
-  'sindrets/diffview.nvim',
+  'dlyongemallo/diffview-plus.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
+  dependencies = { 'rickhowe/diffchar.vim' },
+  opts = {
+    enhanced_diff_hl = true,
+    diffopt = { algorithm = 'histogram' },
+    use_icons = true,
+    view = {
+      default = { layout = 'diff2_horizontal' },
+      merge_tool = { layout = 'diff3_horizontal' },
+    },
+    file_panel = {
+      listing_style = 'tree',
+      win_config = { position = 'left', width = 35 }, -- Use "auto" to fit content
+    },
+  },
   keys = {
     { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iffview' },
     { '<leader>gD', '<cmd>DiffviewClose<cr>', desc = '[G]it [D]iffview Close' },
